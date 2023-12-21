09ad45567a26eeebe9f14258630b1c7c9adfeceb
ayaanlehashi11 <ayaanlemohamed540@gmail.com>
2023-12-20 22:58:29 -0800

    commit

---
+++ src/main/java/com/ayaanle/Trignometry.java
@@ -0,0 +1,17 @@
+package com.ayaanle;
+
+public class Trignometry {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public int angle()
+    {
+        return 0;
+    }
+
+    public int distance()
+    {
+        return 0;
+    }
+}
